e5b2a75dcea4808e4846e8c3abc16c36c469ad6c
Jianing Yang <jianingy.yang@gmail.com>
2016-09-27 15:36:58 +0800

    Bump version: 0.2.1 → 0.3.0

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ do
    local pin, console = {led = settings.pin.led, tact = 3}, false
 
    -- disable serial/uart (console)
-   print('starting nodemcu base system. version: 0.2.1')
+   print('starting nodemcu base system. version: 0.3.0')
    print('Press ENTER or KEY_FLASH to enable the console.')
    uart.on('data', '\r',
            function(data)
